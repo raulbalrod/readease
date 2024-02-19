db78b4fbd6fdd5bcf400a76e84a838745708f573
raulbalrod <119789823+raulbalrod@users.noreply.github.com>
2024-02-19 18:29:25 +0100

    Create a book schema

---
+++ server/src/models/Book.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const  { Schema, model } =  mongoose;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  subtitle: { type: String, required: true },
+  description: { type: String, required: true },
+  categories: { type: Array },
+  rating: { type: Number, min:0, max: 5 , default: 0 },
+  status: { type: Array, enum: ['Leyendo', 'Futuras lecturas', 'Leído'], default: ['Futuras lecturas']},
+  image: {
+    frontImage:  { type: String, required: true },
+  },
+  authors: {
+    name:  { type: String, required: true },
+    img:  { type: String },
+    biography:  { type: String },
+  },
+  editorial: { type: String },
+  pageCount: { type: Number, min: 0, required: true },
+  ebook: { type: String },
+  audiobook: { type: String },
+}, { timestamps: true });
+
+export default model('Book', bookSchema);
