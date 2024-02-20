c9301a73ecd4cde5e5ef461846a5123e9365bf6a
raulbalrod <119789823+raulbalrod@users.noreply.github.com>
2024-02-20 17:13:17 +0100

    Add user schema

---
+++ server/src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['Admin', 'Basic', 'Premium'], default: 'Basic' },
+  bookList: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
+});
+
+export default model('User', userSchema);
