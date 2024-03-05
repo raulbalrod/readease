c5b465a1fd931edff7dfc892bcb59e186c01c8e9
raulbalrod <119789823+raulbalrod@users.noreply.github.com>
2024-03-05 18:03:23 +0100

    Fix problems whit connection to MongoDB

--- server/src/loaders/mongodb-loader.js
+++ server/src/loaders/mongodb-loader.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import logger from "../utils/logger.js";
 
 export default async function (config) {
-  const { clusterUrl } = config;
+  const { url } = config;
 
   try {
-    await mongoose.connect(clusterUrl);
-    logger.info(`Connect to MongoDB at ${clusterUrl} 👌`);
+    await mongoose.connect(url);
+    logger.info(`Connect to MongoDB at ${url} 👌`);
   } catch (err) {
-    logger.error(`Error conecting to MongoDB at ${clusterUrl} 👎.\n ${err}`);
+    logger.error(`Error conecting to MongoDB at ${url} 👎.\n ${err}`);
   }
 }
