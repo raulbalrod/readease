dc4c18b8dd24c48fdd87a3975954632498e830fc
raulbalrod <raulballesterorodriguez@gmail.com>
2024-03-27 11:59:43 +0100

    Attach auth code

--- server/src/middlewares/auth-middleware.js
+++ server/src/middlewares/auth-middleware.js
@@ -3,68 +3,45 @@ import jwt from "jsonwebtoken";
 import logger from "../utils/logger.js";
 import config from "../config.js";
 
-export function checkToken(req, res, next) {
+function verifyToken(req, _res, next) {
   const { authorization } = req.headers;
 
-  if (!authorization) {
-    throw new HttpStatusError(401, "No token provided");
-  }
+  if (!authorization) throw new HttpStatusError(401, "No token provided");
 
   const [_bearer, token] = authorization.split(" ");
 
   try {
     const tokenInfo = jwt.verify(token, config.app.secretKey);
     req.user = tokenInfo;
-    next();
+    return next();
   } catch (err) {
     logger.error(err.message);
     throw new HttpStatusError(401, "Invalid token");
   }
 }
 
+export function checkToken(req, res, next) {
+  verifyToken(req, res, next);
+}
+
 export function checkTokenToEditUserData(req, res, next) {
-  const { authorization } = req.headers;
-
-  if (!authorization) {
-    return res.status(401).send({ message: "No token provided" });
-  }
-
-  const [_bearer, token] = authorization.split(" ");
-
-  try {
-    const tokenInfo = jwt.verify(token, config.app.secretKey);
-    req.user = tokenInfo;
-
+  verifyToken(req, res, () => {
     if (req.params.id !== req.user.userId) {
       return res
         .status(403)
         .send({ message: "You do not have permission to edit this user" });
     }
-
     next();
-  } catch (err) {
-    return res.status(401).send({ message: "Invalid token" });
-  }
+  });
 }
 
 export function isAdmin(req, res, next) {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.status(401).send({ message: "Missing token" });
-  }
-
-  jwt.verify(token.split(" ")[1], config.app.secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Invalid token" });
-    }
-
-    if (decoded.role !== "Admin") {
+  verifyToken(req, res, () => {
+    if (req.user.role !== "Admin") {
       return res
         .status(403)
         .send({ message: "You are not authorized to access this resource" });
     }
-
     next();
   });
 }
@@ -74,9 +51,7 @@ export function checkRole(req, res, next) {
   if (!token) return res.status(401).send({ message: "Missing token" });
 
   jwt.verify(token.split(" ")[1], config.app.secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Invalid token" });
-    }
+    if (err) res.status(401).send({ message: "Invalid token" });
 
     req.decoded = decoded;
     next();
