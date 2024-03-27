734121a3535c6c4b5d4fe68a100e4a9a713e45ff
raulbalrod <raulballesterorodriguez@gmail.com>
2024-03-27 12:11:03 +0100

    Update endpoints whit version

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,8 +3,9 @@ import bookRouter from "./book-router.js";
 import userRouter from "./user-router.js";
 
 const router = express.Router();
+const versionAPI = "v1";
 
-router.use("/books", bookRouter);
-router.use("/users", userRouter);
+router.use(`/${versionAPI}/books`, bookRouter);
+router.use(`/${versionAPI}/users`, userRouter);
 
 export default router;
