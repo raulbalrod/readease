1dded5683435cfb5ab3ad7a2be8bbf8461f997d0
raulbalrod <raulballesterorodriguez@gmail.com>
2024-05-26 10:30:51 +0200

    Create footer

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+import { LEGAL_LINK } from "@/constants/legals"
+import { SOCIAL_MEDIA } from "@/constants/rrss"
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col justify-center items-center gap-4 py-10">
+      <ul className="flex gap-5">
+        {SOCIAL_MEDIA.map((rs) => (
+          <li key={rs.name}>
+            <a href={rs.link}>
+              <i
+                className={`bx ${rs.logo} bx-md text-accent hover:text-accent/85`}
+              ></i>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex gap-5">
+        {LEGAL_LINK.map((rs) => (
+          <li
+            key={rs.name}
+            className="text-accent hover:underline cursor-pointer"
+          >
+            <a href={rs.link} target="_blank">
+              {rs.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <span className="text-accent">
+        ©2023 BookBudy SA. All Rights Reserved Bookbuddy™ is used under license.
+      </span>
+    </footer>
+  )
+}
---
+++ client/src/constants/legals.ts
@@ -0,0 +1,27 @@
+export const LEGAL_LINK = [
+  {
+    title: "Privacy Policy",
+    link: "https://www.instagram.com/bookbuddy_official/",
+    name: "privacy-policy",
+  },
+  {
+    title: "Manage Preferences",
+    link: "https://x.com/bookbuddy_",
+    name: "manage-preferences",
+  },
+  {
+    title: "Term of Use",
+    link: "https://www.tiktok.com/@bookbuddy_",
+    name: "term-of-use",
+  },
+  {
+    title: "Help Center",
+    link: "https://www.youtube.com/@",
+    name: "help-center",
+  },
+  {
+    title: "Corporate Info",
+    link: "https://www.youtube.com/@",
+    name: "corporate-info",
+  },
+]
