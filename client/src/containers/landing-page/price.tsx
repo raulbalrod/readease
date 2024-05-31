02c9b4f80835fa2d328818385e60df63b8f17470
raulbalrod <raulballesterorodriguez@gmail.com>
2024-05-31 20:36:01 +0200

    Create a pricing section from landing

---
+++ client/src/containers/landing-page/price.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import PlanCard from "@/components/PlanCard"
+import { PRICING } from "@/constants/pricing"
+import React, { useState } from "react"
+
+export default function PriceSection() {
+  const [isAnnual, setIsAnnual] = useState(false)
+
+  return (
+    <div
+      id="prices"
+      className="w-full flex flex-col items-center justify-center gap-2 py-16 bg-black/20 bg-custom-linear"
+    >
+      <h1 className="text-secondary font-semibold">Pricing</h1>
+      <h3 className="text-center text-4xl font-semibold">
+        Select the perfect plan
+      </h3>
+      <p className="text-center w-1/2 text-neutral/80 mb-10">
+        Enjoy local titles, bestsellers, and a wide range of new features for
+        the world of reading.
+      </p>
+
+      <div className="flex p-1 gap-4 mb-10 bg-neutral/15 rounded-full">
+        <button
+          className={`px-6 py-1 font-medium rounded-full ${
+            !isAnnual ? "bg-secondary/90 text-neutral" : ""
+          }`}
+          onClick={() => setIsAnnual(false)}
+        >
+          Monthly
+        </button>
+
+        <button
+          className={`px-6 py-1 font-medium rounded-full ${
+            isAnnual ? "bg-secondary text-white" : ""
+          }`}
+          onClick={() => setIsAnnual(true)}
+        >
+          Annually
+        </button>
+      </div>
+
+      <section className="flex justify-between items-start gap-6">
+        {PRICING.map((plan, index) => (
+          <PlanCard key={index} plan={plan} isAnnual={isAnnual} />
+        ))}
+      </section>
+    </div>
+  )
+}
