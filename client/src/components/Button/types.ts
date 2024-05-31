8877fc777a670b8a46ace320fd09a213bc74e429
raulbalrod <raulballesterorodriguez@gmail.com>
2024-05-31 20:38:44 +0200

    Add more variants to Button

--- client/src/components/Button/types.ts
+++ client/src/components/Button/types.ts
@@ -1,5 +1,5 @@
 export interface ButtonProps {
   text: string
   path: string
-  variant?: "default" | "outlined" | "only_text"
+  variant?: "default" | "outlined" | "only_text" | "price" | "secondary"
 }
