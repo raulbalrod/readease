1121db09da60b1862a3190447ef7a9f74d9f8fdb
raulbalrod <raulballesterorodriguez@gmail.com>
2024-05-31 21:07:14 +0200

    Create accordion component by shadcn

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -21,6 +21,20 @@ const config: Config = {
         accent: "#7F6CD5",
         neutral: "#F2F2F2",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],
