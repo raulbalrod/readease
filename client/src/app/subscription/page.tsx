8da032d8f046ee7ae1da4a3ce4740b9012ce70bc
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-01 16:23:40 +0200

    Update

---
+++ client/src/app/subscription/page.tsx
@@ -0,0 +1,7 @@
+export default function SubscribePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-start justify-between">
+      <h1>Subscribe Page</h1>
+    </main>
+  )
+}
