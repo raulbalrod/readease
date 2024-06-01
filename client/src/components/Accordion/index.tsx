888d6b0a03bcd2f352f0053bd070e3465af69e5b
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-01 12:31:25 +0200

    Update Accordion component

--- client/src/components/Accordion/index.tsx
+++ client/src/components/Accordion/index.tsx
@@ -14,7 +14,10 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b border-[#BEB2F4]", className)}
+    className={cn(
+      "border-b border-[#BEB2F4] text-[#BEB2F4] w-[600px]",
+      className,
+    )}
     {...props}
   />
 ))
