5a77d37d5246a1537ee7603f48b525d1bd704e60
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-01 12:34:51 +0200

    Update must sees section

--- client/src/containers/landing-page/must-sees.tsx
+++ client/src/containers/landing-page/must-sees.tsx
@@ -34,37 +34,49 @@ export default function ItemsList() {
   const nextFiveItems = items.slice(10, 15)
 
   return (
-    <div className="flex flex-col gap-6 items-start justify-center mx-auto max-w-screen-lg pl-4">
-      <div className="flex items-center gap-10" style={{ paddingLeft: "40px" }}>
-        {firstFiveItems.map((item: any, index: number) => (
-          <div key={index}>
-            <Image
-              src={item.image.frontImage}
-              alt={`Front image for ${item.title}`}
-              width={250}
-              height={325}
-              style={{ width: "auto", height: "auto" }}
-            />
-          </div>
-        ))}
-      </div>
+    <>
+      <h1 className="text-left text-4xl font-semibold mb-4 w-72">
+        Everything you can't miss
+      </h1>
+      <h3 className="text-left text-lg mb-8 w-[550px]">
+        Enjoy a unique collection of avant-garde literary works, critically
+        praised books in ebook and audiobook format.
+      </h3>
+      <div className="flex flex-col gap-6 items-start justify-center mx-auto max-w-screen-lg pl-4">
+        <div
+          className="flex items-center gap-10"
+          style={{ paddingLeft: "40px" }}
+        >
+          {firstFiveItems.map((item: any, index: number) => (
+            <div key={index}>
+              <Image
+                src={item.image.frontImage}
+                alt={`Front image for ${item.title}`}
+                width={250}
+                height={325}
+                style={{ width: "auto", height: "auto" }}
+              />
+            </div>
+          ))}
+        </div>
 
-      <div
-        className="flex items-start gap-10 mt-16"
-        style={{ paddingLeft: "40px" }}
-      >
-        {nextFiveItems.map((item: any, index: number) => (
-          <div key={index}>
-            <Image
-              src={item.image.frontImage}
-              alt={`Front image for ${item.title}`}
-              width={250}
-              height={325}
-              style={{ width: "auto", height: "auto" }}
-            />
-          </div>
-        ))}
+        <div
+          className="flex items-start gap-10 mt-16"
+          style={{ paddingLeft: "40px" }}
+        >
+          {nextFiveItems.map((item: any, index: number) => (
+            <div key={index}>
+              <Image
+                src={item.image.frontImage}
+                alt={`Front image for ${item.title}`}
+                width={250}
+                height={325}
+                style={{ width: "auto", height: "auto" }}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
