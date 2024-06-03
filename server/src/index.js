fc44d81a567b584246b6ce5c4134042bd33f181e
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-03 09:19:13 +0200

    Add cronjob to dont turn off api

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,17 @@
-import app from './app.js';
-import config from './config.js';
+import app from "./app.js";
+import config from "./config.js";
 
 const { port } = config;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+cron.schedule("*/14 * * * *", async () => {
+  try {
+    const response = await axios.get(`${deployUrl}/books/landing`);
+    console.log(`Ping exitoso a ${deployUrl}: ${response.data}`);
+  } catch (error) {
+    console.error(`Error al hacer ping: ${error.message}`);
+  }
 });
