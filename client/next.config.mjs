30e181cc09f5921632c3060f758fea05d6779d0e
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-04 19:51:54 +0200

    Add new allowed domains

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["static.fnac-static.com", "m.media-amazon.com"],
+    domains: [
+      "static.fnac-static.com",
+      "m.media-amazon.com",
+      "imagessl3.casadellibro.com",
+      "hips.hearstapps.com",
+      "medios.lamarmota.es",
+      "unavatar.io",
+    ],
   },
 }
 
