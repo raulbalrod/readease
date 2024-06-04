8739b453bcf2cfb68a5be6df1c9381de160a1ecc
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-04 20:06:37 +0200

    Implement auth provider in RootLayout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import "boxicons/css/boxicons.min.css"
+import { AuthProvider } from "@/contexts/AuthContext"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AuthProvider>
   )
 }
