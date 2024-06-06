61487d6130819e7604f443c388797f28320a2bcc
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-06 19:21:12 +0200

    Update my list from home page to dont render if length < 2

--- client/src/containers/home/Categorie.tsx
+++ client/src/containers/home/Categorie.tsx
@@ -24,62 +24,55 @@ export default function Categorie({
   const [token, setToken] = useState<string | null>(null)
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const storedToken = localStorage.getItem("token")
-      if (!storedToken) {
-        router.push("/sign-in")
-      } else {
-        setToken(storedToken)
-      }
+    const storedToken = localStorage.getItem("token")
+    if (!storedToken) {
+      router.push("/sign-in")
     } else {
-      router.push("/landing-page")
+      setToken(storedToken)
     }
   }, [router])
 
   useEffect(() => {
+    if (!token) return
+
     const fetchBooks = async () => {
-      if (token) {
-        try {
-          const response = await fetch(
-            `https://bookbuddy-v7ra.onrender.com/v1/${endpoint}`,
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
+      try {
+        const response = await fetch(
+          `https://bookbuddy-v7ra.onrender.com/v1/${endpoint}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
             },
-          )
-          if (!response.ok) {
-            throw new Error("Error al obtener los libros")
-          }
-          const data = await response.json()
-          setBooks(data.slice(0, 18))
-          setLoading(false)
-        } catch (error: any) {
-          setError(error.message)
-          setLoading(false)
-        }
+          },
+        )
+        if (!response.ok) throw new Error("Error al obtener los libros")
+        const data = await response.json()
+        setBooks(data.slice(0, 18))
+      } catch (error: any) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
       }
     }
 
     fetchBooks()
   }, [token, endpoint])
 
-  const renderHeader = () => {
-    if (endpoint === `users/${username}/books`) {
-      return (
-        <div className="flex items-center justify-between w-full">
-          <h1 className="font-bold m-4 text-2xl">{title}</h1>
-          <a href={`/books/${username}/list`}>
-            <p className="font-bold m-4 text-2xl flex items-center cursor-pointer">
-              See all<i className="bx bx-chevron-right bx-sm"></i>
-            </p>
-          </a>
-        </div>
-      )
-    } else {
-      return <h1 className="font-bold m-4 text-2xl w-full">{title}</h1>
-    }
-  }
+  const renderHeader = () => (
+    <div className="flex items-center justify-between w-full">
+      <h1 className="font-bold m-4 text-2xl">{title}</h1>
+      {endpoint === `users/${username}/books` && (
+        <a
+          href={`/books/${username}/list`}
+          className="font-bold m-4 text-2xl flex items-center cursor-pointer"
+        >
+          See all<i className="bx bx-chevron-right bx-sm"></i>
+        </a>
+      )}
+    </div>
+  )
+
+  if (endpoint === `users/${username}/books` && books.length < 2) return null
 
   return (
     <>
@@ -92,7 +85,7 @@ export default function Categorie({
         className="w-[100%]"
       >
         <CarouselContent>
-          {books.slice(0, 20).map((book, index) => (
+          {books.slice(0, 20).map((book) => (
             <CarouselItem key={book._id} className="basis-1/10">
               <div className="p-1">
                 <BookWhitLink id={book._id} title={book.title} />
