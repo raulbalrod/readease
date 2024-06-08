94f922275142f06abf90208235e3d9f9ef902b03
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-08 18:10:38 +0200

    Make book image responsive

--- client/src/containers/home/Book.tsx
+++ client/src/containers/home/Book.tsx
@@ -10,11 +10,22 @@ export default function BookWhitLink({ id, title }: BookWhitLinkProps) {
   return (
     <Link href={`/book/${id}`}>
       <div
-        className="relative flex flex-col items-center justify-center gap-4 bg-cover bg-center text-neutral"
+        className="relative flex-col items-center justify-center gap-4 bg-cover bg-center text-neutral hidden md:flex"
         style={{
           backgroundImage: `url('/books/${formatTitle(title)}.jpg')`,
           width: 300,
           height: 460,
+        }}
+      >
+        <div className="absolute inset-0"></div>
+      </div>
+
+      <div
+        className="relative flex flex-col items-center justify-center gap-4 bg-cover bg-center text-neutral md:hidden"
+        style={{
+          backgroundImage: `url('/books/${formatTitle(title)}.jpg')`,
+          width: 150,
+          height: 260,
         }}
       >
         <div className="absolute inset-0"></div>
