30bba9b3ddb115325924d77abe9f7ad06d9d2a81
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-09 22:22:22 +0200

    Create my list page

---
+++ client/src/containers/mylist/BookImage.tsx
@@ -0,0 +1,38 @@
+import { formatTitle } from "@/utils/formatTitle"
+import Link from "next/link"
+
+interface BookImageWhitLinkProps {
+  id: string
+  title: string
+}
+
+export default function BookImageWhitLink({
+  id,
+  title,
+}: BookImageWhitLinkProps) {
+  return (
+    <Link href={`/book/${id}`}>
+      <div
+        className="relative flex-col items-center justify-center gap-4 bg-cover bg-center text-neutral hidden md:flex"
+        style={{
+          backgroundImage: `url('/books/${formatTitle(title)}.jpg')`,
+          width: 200,
+          height: 300,
+        }}
+      >
+        <div className="absolute inset-0"></div>
+      </div>
+
+      <div
+        className="relative flex flex-col items-center justify-center gap-4 bg-cover bg-center text-neutral md:hidden"
+        style={{
+          backgroundImage: `url('/books/${formatTitle(title)}.jpg')`,
+          width: 150,
+          height: 260,
+        }}
+      >
+        <div className="absolute inset-0"></div>
+      </div>
+    </Link>
+  )
+}
