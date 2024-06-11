16ce9d4567330a74caac95d74f3eeb08a38335fa
raulbalrod <raulballesterorodriguez@gmail.com>
2024-06-11 19:07:26 +0200

    Fix errors

--- client/src/app/sign-in/layout.tsx
+++ client/src/app/sign-in/layout.tsx
@@ -1,15 +1,5 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import Footer from "@/components/Footer"
 import HeaderSignIn from "@/components/Header/header-login"
-
-const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
-  title: "Bookbuddy | Your Reading Companion",
-  description:
-    "Discover a new way to enjoy reading with BookBuddy, the ultimate app for book lovers. With BookBuddy, you have access to a wide selection of books in eBook and audiobook formats, so you can read or listen to your favorite titles anytime, anywhere.",
-}
 
 export default function SignInLayout({
   children,
@@ -17,12 +7,10 @@ export default function SignInLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <HeaderSignIn />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <HeaderSignIn />
+      {children}
+      <Footer />
+    </>
   )
 }
